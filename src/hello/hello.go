760078c3a6f2cc99eb780f6b6d96387e3cc60440
Siso Matheus <sisomd69@gmail.com>
2024-06-17 19:08:56 -0300

    criando funcoes

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,16 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	fmt.Println("1- Iniciar Monitoramento")
+	exibeMenu()
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 3: 
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+	default:
+		fmt.Println("Não reconheço este comando")
+		os.Exit(-1)
+	}
+}
+
+func leEExibeIntroducao() string{ 
+	fmt.Println("Digite o seu nome:")
+	var nomeLido string
+	fmt.Scan(&nomeLido)
+
+	return nomeLido
+}
+
+func exibeMenu() {
+	nome := leEExibeIntroducao()
+	fmt.Println("\nOlá Sr(a)", nome,"o que você gostaria de fazer hoje?")
+	fmt.Println("\n1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("3- Sair do Programa")
+}
 
-	var comando int
-	fmt.Scan(&comando)
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
 
-	fmt.Println("O comando escolhido foi: ", comando)
-	fmt.Println("E o endereço dela é:", &comando)
+	return comandoLido
 }
